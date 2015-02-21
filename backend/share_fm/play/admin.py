d84e8438478a8fb03d5ffc67738a423954c3652f
Igor Kucher <igor.k.843@gmail.com>
2015-02-21 20:14:54 +0200

    add admin.py

--- backend/share_fm/play/admin.py
+++ backend/share_fm/play/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlayList, PlayListItem, UserPlayList, UserPlayListItem
+from .models import Genre
+
+
+class PlayListAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlayListItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserPlayListAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserPlayListItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(PlayList, PlayListAdmin)
+admin.site.register(PlayListItem, PlayListItemAdmin)
+admin.site.register(UserPlayList, UserPlayListAdmin)
+admin.site.register(UserPlayListItem, UserPlayListItemAdmin)
+admin.site.register(Genre, GenreAdmin)
