d44c6f9b18e9ffab8f26f0068d69bca0c57dc37c
Igor Kucher <igor.k.843@gmail.com>
2015-02-21 23:00:11 +0200

    add dummy views urls

--- backend/share_fm/player/urls.py
+++ backend/share_fm/player/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('share_fm.player.views',
     url(r'^$', 'player_index', name='player_index'),
-    url(r'^$', 'player_detail', name='player_detail'),
+    # url(r'^$', 'player_detail', name='player_detail'),
+    # url(r'player/create^$', 'player_create', name='player_create'),
 )
--- backend/share_fm/urls.py
+++ backend/share_fm/urls.py
@@ -5,5 +5,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', include('share_fm.player.urls')),
 
+    url(r'', include('social_auth.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
